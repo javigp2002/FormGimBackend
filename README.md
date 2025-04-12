c7ab1d4260612e4083ddf80343a5d80cf6415c73
Javier G Peregrin <javigp2002@gmail.com>
2025-04-12 15:59:24 +0200

    :sparkles: Init

---
+++ README.md
@@ -0,0 +1,28 @@
+# Form Gim Backend
+Backend to complement Form Gim Android App.
+
+## Project setup
+
+```bash
+$ npm install
+```
+
+## Compile and run the project
+
+```bash
+# development
+$ npm run start
+
+# watch mode
+$ npm run start:dev
+
+# production mode
+$ npm run start:prod
+```
+
+- Author - Javier González Peregrín | [GitHub](https://github.com/javigp2002)
+- Author - Ismael Tengo Rodríguez
+
+## License
+
+- GNU GENERAL PUBLIC LICENSE
---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log', 'fatal'],
+  });
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(process.env.SERVER_PORT || 3000);
+}
+bootstrap();
