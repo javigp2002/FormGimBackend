85024d23448897a48670527fdc7a348d5f5d74a3
ismeh <gadeismael@hotmail.com>
2025-06-07 18:16:42 +0200

    ✨ Add: Google login entrypoint

--- src/api/auth/controller/auth.login.entrypoint.ts
+++ src/api/auth/controller/auth.login.entrypoint.ts
@@ -1,17 +1,19 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { SingInDto } from './dto/sing_in.dto';
 import { LoginUsecase } from '../../../domain/use_cases/auth/login.usecase';
+import { GoogleLoginUsecase } from '../../../domain/use_cases/auth/google-login.usecase';
 import { SingInResponseDto } from './dto/sing_in.response_dto';
+import { GoogleSingInDto } from './dto/google_sing_in.dto';
+import { UserModel } from '../../../domain/model/user.model';
 
 @Controller()
 export class AuthLoginEntrypoint {
-	constructor(private loginUseCase: LoginUsecase) {
+	constructor(private loginUseCase: LoginUsecase, private googleLoginUseCase: GoogleLoginUsecase) {
 	}
 
 	@HttpCode(HttpStatus.OK)
 	@Post('login')
-	async singIn(@Body() singInDto: SingInDto): Promise<SingInResponseDto> {
-		const result = await this.loginUseCase.run(singInDto.user_name, singInDto.password);
-		return new SingInResponseDto(result.id, result.name);
+	async singIn(@Body() googleToken: GoogleSingInDto): Promise<UserModel> {
+		return this.googleLoginUseCase.run(googleToken.token);
 	}
 }
